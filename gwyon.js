4ccb1b6a23824d5eb03163d3a3b1bdc66e931287
Nicolas Miller <nicolasmiller@gmail.com>
2012-10-03 23:11:53 -0500

    personal site changes

--- gwyon.js
+++ gwyon.js
@@ -5,6 +5,7 @@ var START_Y = 0;
 var newBlock = true;
 var current;
 var current_type;
+var current_color;
 var current_x;
 var current_y;
 var score = 0;
@@ -109,11 +110,11 @@ function lostGame() {
 }
 
 function on(x, y) {
-	$("#" + String(x) + "_" + String(y)).addClass("on");
+	$("#" + String(x) + "_" + String(y)).addClass("on").css('background-color', current_color);
 }
 
 function off(x, y) { 
-	$("#" + String(x) + "_" + String(y)).removeClass("on");
+	$("#" + String(x) + "_" + String(y)).removeClass("on").css('background-color', '#f0f0f0');
 }
 
 function isOn(x, y) {
@@ -308,6 +309,11 @@ function initializeKeyboardInput() {
 }
 
 function main() {
+    function random_gray() {
+        var grays = ['#333', '#444', '#555', '#666', '#777', '#888', '#999'];
+        return grays[Math.floor(Math.random() * grays.length)];
+    }
+
 	if(newBlock) {
 		if(lostGame()) {
 			fillScreen();
@@ -318,6 +324,8 @@ function main() {
 		removeFullRows();
 		updateScore();
 		current_type = randomBlock();
+        current_color = random_gray();
+        console.log(current_color);
 		current = jQuery.extend(true, [], blocks[current_type]);
 		drawBlock(START_X, START_Y);
 		current_x = START_X;
